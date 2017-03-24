649f596b3424733fcaed3e0af9664d57fe84be35
Jason Otero <jason@sidecarsinc.com>
2017-03-24 11:13:18 -0500

    Pep 8 cleanup

--- setup.py
+++ setup.py
@@ -12,23 +12,25 @@ CLASSIFIERS = [
                 'Operating System :: OS Independent',
                 'Environment :: Web Environment',
                 'Framework :: Django',
-                'Programming Language :: Python',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
                 'Topic :: Software Development :: Libraries :: Python Modules' 
               ]
 
 KEYWORDS = 'sorting sortable queryset sql pagination django'
 
 setup(name='django-sortable',
-    version='0.3.1',
-    description='Flexible sorting for Django applications',
-    long_description=LONG_DESCRIPTION,
-    author='Drew Yeaton',
-    author_email='xeeton@gmail.com',
-    url='http://github.com/drewyeaton/django-sortable',
-    packages=find_packages(),    
-    platforms=['Platform Independent'],
-    license='GPLv3',
-    classifiers=CLASSIFIERS,
-    keywords=KEYWORDS,
-    requires = ['django'],
-)
+      version='0.3.1',
+      description='Flexible sorting for Django applications',
+      long_description=LONG_DESCRIPTION,
+      author='Drew Yeaton',
+      author_email='xeeton@gmail.com',
+      url='http://github.com/drewyeaton/django-sortable',
+      packages=find_packages(),
+      platforms=['Platform Independent'],
+      license='GPLv3',
+      classifiers=CLASSIFIERS,
+      keywords=KEYWORDS,
+      requires=['django'],
+      )
