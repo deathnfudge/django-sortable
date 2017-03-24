fdf8819835cca4e9ce3c37d32cc902a5247c025d
Jason Otero <jason@sidecarsinc.com>
2017-03-24 11:19:40 -0500

    Fixed more pep 8 issues

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ CLASSIFIERS = [
                 'Programming Language :: Python :: 2.7',
                 'Programming Language :: Python :: 3.3',
                 'Programming Language :: Python :: 3.4',
-                'Topic :: Software Development :: Libraries :: Python Modules' 
-              ]
+                'Topic :: Software Development :: Libraries :: Python Modules']
 
 KEYWORDS = 'sorting sortable queryset sql pagination django'
 
